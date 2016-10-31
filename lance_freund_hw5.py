19971081f35e0dc17b0053ba43a2e8b29cf5b93d
Lance Freund <lancefreund@mail.weber.edu>
2016-10-30 23:34:14 -0600

    Most of code. Need comments

--- lance_freund_hw5.py
+++ lance_freund_hw5.py
@@ -1,2 +1,34 @@
 #This is homework 5
 #This is my 1st python assignment
+
+#!/usr/bin/env python3
+
+def GetInput():
+    PIN = input()
+    return PIN
+
+count = 1
+
+while True:
+    count = count + 1
+    print("Please enter your PIN:")
+    PIN = GetInput()
+    if PIN == "1234":
+        print("Your PIN is correct")
+        break
+    elif len(PIN) < 4:
+        print("Invalid PIN length. The correct format is: 9999")
+    elif len(PIN) > 4:
+        print("Invalid PIN length. The correct format is: 9999")
+    else:
+        print("Your PIN is incorrect")
+   
+    if not PIN.isdigit():
+        print("Invalid PIN character. Correct format is: 9999")
+
+    if count > 3:
+        print("Your bank card is blocked")
+        break
+        exit(1)
+
+exit(0)
